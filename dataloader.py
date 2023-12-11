dad6a6f3b0c1d3ddedc764d297d1a86e5a59f380
lizhirun <1115926058@qq.com>
2023-12-11 09:22:10 +0800

    2023-12-11 morning

--- dataloader.py
+++ dataloader.py
@@ -186,11 +186,57 @@ def h5py_to_window(filename):
     return emg, label
 
 
+def split_window_ration(emg, label, ratio, window_overlap=200):
+    window_size = 400
+    denominator = sum(ratio)
+
+    train_emg, train_label, val_emg, val_label, eval_emg, eval_label = [], [], [], [], [], []
+
+    for i in range(len(label)):
+        data_len = len(emg[i])
+        train_len = int(data_len * ratio[0] / denominator)
+        val_len = int(data_len * ratio[1] / denominator)
+
+        emg_type = np.array(emg[i][:train_len])
+        window_count = 0
+        for j in range(0, len(emg_type) - window_size, window_size - window_overlap):
+            train_emg.append(emg_type[j : j + window_size])
+            train_label.append(label[i])
+            window_count += 1
+        # print('{} train window data found in type {} emg signal.'.format(window_count, label[i]))
+
+        emg_type = np.array(emg[i][train_len : train_len + val_len])
+        window_count = 0
+        for j in range(0, len(emg_type) - window_size, window_size):
+            val_emg.append(emg_type[j : j + window_size])
+            val_label.append(label[i])
+            window_count += 1
+        # print('{} val window data found in type {} emg signal.'.format(window_count, label[i]))
+
+        emg_type = np.array(emg[i][train_len + val_len :])
+        window_count = 0
+        for j in range(0, len(emg_type) - window_size, window_size):
+            eval_emg.append(emg_type[j : j + window_size])
+            eval_label.append(label[i])
+            window_count += 1
+        # print('{} eval window data found in type {} emg signal.'.format(window_count, label[i]))
+
+    train_emg = np.array(train_emg)
+    train_label = np.array(train_label)
+    val_emg = np.array(val_emg)
+    val_label = np.array(val_label)
+    eval_emg = np.array(eval_emg)
+    eval_label = np.array(eval_label)
+    return train_emg, train_label, val_emg, val_label, eval_emg, eval_label
+
+
 if __name__ == '__main__':
     filename = 'D:/Download/Datasets/Ninapro/DB2/S1/S1_E1_A1.mat'
     h5_filename = 'dataset/window_400_300.h5'
     emg, label = load_emg_label_from_file(filename)
-    window_to_h5py(emg, label, h5_filename, window_overlap=300)
-    emg, label = h5py_to_window(h5_filename)
-    print(emg.shape)
-    print(label.shape)
+    # window_to_h5py(emg, label, h5_filename, window_overlap=300)
+    # emg, label = h5py_to_window(h5_filename)
+    # print(emg.shape)
+    # print(label.shape)
+
+    split_window_ration(emg, label, (4, 1, 1))
